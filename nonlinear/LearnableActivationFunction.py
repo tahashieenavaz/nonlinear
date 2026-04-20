044fd2bc725d9495b9c32fb99c877582cc20d8ee
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-20 19:13:08 +0200

    removes extra method

--- nonlinear/LearnableActivationFunction.py
+++ nonlinear/LearnableActivationFunction.py
@@ -1,6 +1,5 @@
-import torch
 from .ActivationFunction import ActivationFunction
 
+
 class LearnableActivationFunction(ActivationFunction):
-    def get_shape(self, x: torch.Tensor):
-        return (1, -1) + (1,) * (x.ndim - 2)
+    pass
