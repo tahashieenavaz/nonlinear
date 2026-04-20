c07e3d6bb18f50e9692fe650f8ee249a5023a40f
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-20 18:26:11 +0200

    fixes StarReLU default values

--- nonlinear/ChannelBasedActivationFunction.py
+++ nonlinear/ChannelBasedActivationFunction.py
@@ -23,14 +23,18 @@ class ChannelBasedActivationFunction(LearnableActivationFunction):
 
         self.num_channels = x.shape[1]
         device = x.device
+        dtype = x.dtype 
+        
         for index, attr in enumerate(attrs):
             if index < len(values) and values[index] is not None:
                 default_value = float(values[index])
             else:
                 default_value = 1.0
-            param = torch.nn.Parameter(torch.full((self.num_channels,), default_value))
-            param = param.to(device)
+                
+            init_tensor = torch.full((self.num_channels,), default_value, device=device, dtype=dtype)
+            param = torch.nn.Parameter(init_tensor)
             setattr(self, attr, param)
+            
         self._initialized = True
 
     def parameter_shape(self, x: torch.Tensor) -> tuple | None:
--- nonlinear/learnable/StarReLU.py
+++ nonlinear/learnable/StarReLU.py
@@ -9,7 +9,7 @@ class StarReLU(ChannelBasedActivationFunction):
         self.b = None
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        self.initialize(x, ["a", "b"])
+        self.initialize(x, ["a", "b"], [0.8944, -0.4472])
         a = self.a.view(self.parameter_shape(x))
         b = self.b.view(self.parameter_shape(x))
         return a * torch.relu(x).pow(2) + b
