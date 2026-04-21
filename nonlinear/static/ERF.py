070166875258b09b7407b74bdbdd7afc5d3787cc
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-21 12:12:37 +0200

    improves ERF implementation

--- nonlinear/static/ERF.py
+++ nonlinear/static/ERF.py
@@ -1,10 +1,11 @@
 import torch
+from ..ActivationFunction import ActivationFunction
 
 
-class ERF(torch.nn.Module):
-    def __init__(self, alpha: float=1.0):
+class ERF(ActivationFunction):
+    def __init__(self, *, alpha: float = 1.0):
         super().__init__()
         self.alpha = torch.nn.Parameter(torch.tensor(alpha))
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return x * torch.erf(self.alpha * x)
