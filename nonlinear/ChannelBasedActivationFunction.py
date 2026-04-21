1e1d3c16ccab6f09ebc950db86cfbd2e4d61c1fa
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-21 12:31:48 +0200

    adds get_parameter method

--- nonlinear/ChannelBasedActivationFunction.py
+++ nonlinear/ChannelBasedActivationFunction.py
@@ -3,5 +3,10 @@ from .LearnableActivationFunction import LearnableActivationFunction
 
 
 class ChannelBasedActivationFunction(LearnableActivationFunction):
-    def get_shape(self, x: torch.Tensor):
+    def get_shape(self, x: torch.Tensor) -> tuple[int, ...]:
         return (1, -1) + (1,) * (x.ndim - 2)
+
+    def get_parameter(
+        self, channels: int, initial_value: float | int
+    ) -> torch.nn.Parameter:
+        return torch.nn.Parameter(torch.full((channels,), float(initial_value)))
