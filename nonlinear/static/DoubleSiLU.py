6e3e8834c4d7f13841fea682ef057a58878e8783
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-21 12:08:30 +0200

    removes extra args

--- nonlinear/static/DoubleSiLU.py
+++ nonlinear/static/DoubleSiLU.py
@@ -3,8 +3,8 @@ from ..ActivationFunction import ActivationFunction
 
 
 class DoubleSiLU(ActivationFunction):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        super().__init__()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         a = 1 + torch.exp(-x)
