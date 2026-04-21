a02872a447d776073f955eff7e26aa2ef4ba5ff1
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-21 12:15:03 +0200

    improves IpLU implementation

--- nonlinear/static/IpLU.py
+++ nonlinear/static/IpLU.py
@@ -3,10 +3,10 @@ from ..ActivationFunction import ActivationFunction
 
 
 class IpLU(ActivationFunction):
-    def __init__(self, alpha: float = 1.4):
+    def __init__(self, *, alpha: float = 1.4):
         super().__init__()
         self.alpha = alpha
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         delta = 1 + x.abs().pow(self.alpha)
         return torch.where(x >= 0, x, x / delta)
