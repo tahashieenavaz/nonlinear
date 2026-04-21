55dfcb52b2cff4078159b8beb1b70b752c0151a0
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-21 12:10:57 +0200

    adds DiffELU args

--- nonlinear/static/DiffELU.py
+++ nonlinear/static/DiffELU.py
@@ -3,10 +3,10 @@ from ..ActivationFunction import ActivationFunction
 
 
 class DiffELU(ActivationFunction):
-    def __init__(self):
+    def __init__(self, *, a: float = 0.3, b: float = 0.1):
         super().__init__()
-        self.a = 0.3
-        self.b = 0.1
+        self.a = a
+        self.b = b
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         delta = x * x.exp() - self.b * torch.exp(self.b * x)
