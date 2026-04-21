bd7ae732f5dc973c22349dbf7c7c06302e2deacb
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-21 12:12:03 +0200

    removes extra args

--- nonlinear/static/Elliot.py
+++ nonlinear/static/Elliot.py
@@ -3,8 +3,8 @@ from ..ActivationFunction import ActivationFunction
 
 
 class Elliot(ActivationFunction):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        super().__init__()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return 0.5 + torch.div(0.5 * x, 1 + torch.abs(x))
