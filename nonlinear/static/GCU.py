6fcc9fea805ac1a1fd3df25cc28e85deeca58589
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-21 12:13:41 +0200

    adds GCU type hints

--- nonlinear/static/GCU.py
+++ nonlinear/static/GCU.py
@@ -6,5 +6,5 @@ class GCU(ActivationFunction):
     def __init__(self):
         super().__init__()
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return x * torch.cos(x)
