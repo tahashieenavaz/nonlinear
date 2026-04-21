a0b089b8fe1b8585d119e0780b9122d33a0b91f5
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-21 12:15:20 +0200

    removes LaLU extra args

--- nonlinear/static/LaLU.py
+++ nonlinear/static/LaLU.py
@@ -3,8 +3,8 @@ from ..ActivationFunction import ActivationFunction
 
 
 class LaLU(ActivationFunction):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        super().__init__()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         phi_laplace = torch.where(x >= 0, 1 - 0.5 * torch.exp(-x), 0.5 * torch.exp(x))
