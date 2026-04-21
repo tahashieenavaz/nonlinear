32c7ca941434038536943ef7936a6c86dfe92e91
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-21 12:24:03 +0200

    adds enum type for PoLU

--- nonlinear/static/PoLU.py
+++ nonlinear/static/PoLU.py
@@ -1,15 +1,19 @@
 import torch
+from enum import Enum
 from ..ActivationFunction import ActivationFunction
 
 
-class PoLU(ActivationFunction):
-    def __init__(self, alpha: float = 1.5):
-        super().__init__()
-        if alpha not in [1, 1.5, 2]:
-            raise ValueError(f"PoLU alpha must be in [1, 2, 1.5]. Got {alpha}.")
+class PoLUAlphaValues(float, Enum):
+    ONE = 1
+    ONE_HALF = 1.5
+    TWO = 2
 
+
+class PoLU(ActivationFunction):
+    def __init__(self, *, alpha: PoLUAlphaValues = 1.5):
+        super().__init__()
         self.alpha = alpha
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         delta = 1 - x
         return torch.where(x >= 0, x, delta.pow(-self.alpha) - 1)
