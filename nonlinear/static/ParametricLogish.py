e081a7be0fb87d89fd78a17afad97cdcf60dbe9b
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-21 12:18:04 +0200

    improves parametric logish implementation

--- nonlinear/static/ParametricLogish.py
+++ nonlinear/static/ParametricLogish.py
@@ -1,8 +1,9 @@
 import torch
+from ..ActivationFunction import ActivationFunction
 
 
-class ParametricLogish(torch.nn.Module):
-    def __init__(self, alpha: float = 1.0, beta: float = 10.0):
+class ParametricLogish(ActivationFunction):
+    def __init__(self, *, alpha: float = 1.0, beta: float = 10.0):
         super().__init__()
         self.alpha = alpha
         self.beta = beta
