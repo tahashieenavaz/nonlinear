0fb56a8734aa620a94c41595288a99f027a5899c
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-21 12:14:35 +0200

    improves gish type hints

--- nonlinear/static/Gish.py
+++ nonlinear/static/Gish.py
@@ -6,6 +6,6 @@ class Gish(ActivationFunction):
     def __init__(self):
         super().__init__()
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         a = -torch.exp(x)
         return x * torch.log(2 - torch.exp(a))
