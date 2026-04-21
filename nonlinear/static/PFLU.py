1658c1df3a339390e2f94d8cd53a135f30a336ab
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-21 12:18:18 +0200

    adds type hints to PFLU

--- nonlinear/static/PFLU.py
+++ nonlinear/static/PFLU.py
@@ -6,6 +6,6 @@ class PFLU(ActivationFunction):
     def __init__(self):
         super().__init__()
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         delta = x / (torch.sqrt(1 + x.pow(2)))
         return x * 0.5 * (1 + delta)
