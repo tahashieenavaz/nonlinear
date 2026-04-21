6eed1f6eda1a28d6eff2a0c21870bb1bd238be5d
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-21 12:15:56 +0200

    improves loglogish implementation

--- nonlinear/static/LogLogish.py
+++ nonlinear/static/LogLogish.py
@@ -1,10 +1,11 @@
 import torch
+from ..ActivationFunction import ActivationFunction
 
 
-class LogLogish(torch.nn.Module):
+class LogLogish(ActivationFunction):
     def __init__(self):
         super().__init__()
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         a = -torch.exp(x)
         return x * (1 - torch.exp(a))
