5ba1216670c37e2d6c7ee48321e6e30f3433733b
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-27 13:59:41 +0200

    adds AReLU jax

--- nonlinear/learnable/AReLU.py
+++ nonlinear/learnable/AReLU.py
@@ -8,16 +8,16 @@ class AReLU(LearnableActivationFunction):
         *,
         a: float = 0.9,
         b: float = 2.0,
-        clamp_min: float = 0.0,
-        clamp_max: float = 0.99,
+        alpha: float = 0.0,
+        beta: float = 0.99,
     ):
         super().__init__()
-        self.clamp_min = clamp_min
-        self.clamp_max = clamp_max
+        self.alpha = alpha
+        self.beta = beta
         self.a = torch.nn.Parameter(torch.tensor(a))
         self.b = torch.nn.Parameter(torch.tensor(b))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        negative_slope = torch.clamp(self.a, min=self.clamp_min, max=self.clamp_max)
+        negative_slope = torch.clamp(self.a, min=self.alpha, max=self.beta)
         positive_slope = 1.0 + torch.sigmoid(self.b)
         return torch.where(x >= 0, positive_slope * x, negative_slope * x)
