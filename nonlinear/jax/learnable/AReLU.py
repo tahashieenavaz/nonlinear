1eae79f919fa4bf3e8369bca780d9e7e0583946f
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-27 14:05:17 +0200

    updates AReLU jax

--- nonlinear/jax/learnable/AReLU.py
+++ nonlinear/jax/learnable/AReLU.py
@@ -5,13 +5,13 @@ from flax import linen as nn
 class AReLU(nn.Module):
     a: float = 0.9
     b: float = 2.0
-    alef: float = 0.0
-    be: float = 0.99
+    alpha: float = 0.0
+    beta: float = 0.99
 
     @nn.compact
     def __call__(self, x: jax.Array) -> jax.Array:
         a = self.param("a", lambda key: jax.numpy.array(self.a_init))
         b = self.param("b", lambda key: jax.numpy.array(self.b_init))
-        negative_slope = jax.numpy.clip(a, self.alef, self.be)
+        negative_slope = jax.numpy.clip(a, self.alpha, self.beta)
         positive_slope = 1.0 + jax.nn.sigmoid(b)
         return jax.numpy.where(x >= 0, positive_slope * x, negative_slope * x)
