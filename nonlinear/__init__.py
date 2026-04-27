e9c9bd6c9783575679272a69a5621197957ccbbd
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-27 14:33:17 +0200

    moves existing code into pytorch sub module

--- nonlinear/__init__.py
+++ nonlinear/__init__.py
@@ -1,6 +1,1 @@
-from .ActivationFunction import ActivationFunction
-from .ChannelBasedActivationFunction import ChannelBasedActivationFunction
-from .LearnableActivationFunction import LearnableActivationFunction
-from .channel_based import *
-from .static import *
-from .learnable import *
+# __version__ = "0.0.1"
