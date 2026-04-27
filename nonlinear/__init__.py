b34c1f916939920641eb61923c854d28d446f4c6
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-27 22:27:03 +0200

    moves torch back to the root

--- nonlinear/__init__.py
+++ nonlinear/__init__.py
@@ -1,1 +1,6 @@
-# __version__ = "0.0.1"
+from .ActivationFunction import ActivationFunction
+from .ChannelBasedActivationFunction import ChannelBasedActivationFunction
+from .LearnableActivationFunction import LearnableActivationFunction
+from .channel_based import *
+from .static import *
+from .learnable import *
