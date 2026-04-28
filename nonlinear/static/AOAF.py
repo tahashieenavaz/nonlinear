fabce4484c6602b9e33b4a5210746a807dc3b1ff
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-28 12:07:08 +0200

    adds aoaf comment

--- nonlinear/static/AOAF.py
+++ nonlinear/static/AOAF.py
@@ -3,6 +3,18 @@ from ..functional import aoaf
 from ..ActivationFunction import ActivationFunction
 
 
+"""
+Paper: https://doi.org/10.3390/electronics11223799
+
+Formula: f(z) = ReLU(z - b*a) + c*a
+    
+Variables:
+    - x: Input tensor.
+    - a: Channel-wise / Neuron-wise mean of the input.
+    - b, c: Fixed hyperparameters (Recommended: b=c=0.17).
+"""
+
+
 class AOAF(ActivationFunction):
     def __init__(self, *, b: float = 0.17, c: float = 0.17, inplace: bool = False):
         super().__init__()
