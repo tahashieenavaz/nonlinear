b7843cddaeba2fec4fd69d4853955092dd5f1d11
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-28 12:07:35 +0200

    adds aoaf comment on functional file

--- nonlinear/functional/aoaf.py
+++ nonlinear/functional/aoaf.py
@@ -1,5 +1,16 @@
 import torch
 
+"""
+Paper: https://doi.org/10.3390/electronics11223799
+
+Formula: f(z) = ReLU(z - b*a) + c*a
+    
+Variables:
+    - x: Input tensor.
+    - a: Channel-wise / Neuron-wise mean of the input.
+    - b, c: Fixed hyperparameters (Recommended: b=c=0.17).
+"""
+
 
 def aoaf(x: torch.Tensor, *, b: float = 0.17, c: float = 0.17, inplace: bool = False):
     reduce_dims = [d for d in range(x.ndim) if d != 1]
