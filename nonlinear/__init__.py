dfdff94a5ef59c68f654299c35d4d86ecb607809
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-28 12:08:08 +0200

    updates init file

--- nonlinear/__init__.py
+++ nonlinear/__init__.py
@@ -1,6 +1,5 @@
 from .ActivationFunction import ActivationFunction
 from .ChannelBasedActivationFunction import ChannelBasedActivationFunction
 from .LearnableActivationFunction import LearnableActivationFunction
-from .channel_based import *
 from .static import *
 from .learnable import *
