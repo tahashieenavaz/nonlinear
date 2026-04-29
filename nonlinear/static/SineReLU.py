d9a1985eca8ca40aa7accb187f55447dc6cba3df
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-29 11:18:38 +0200

    makes sinerelu input keyword only

--- nonlinear/static/SineReLU.py
+++ nonlinear/static/SineReLU.py
@@ -3,7 +3,7 @@ from ..ActivationFunction import ActivationFunction
 
 
 class SineReLU(ActivationFunction):
-    def __init__(self, epsilon: float = 0.0025):
+    def __init__(self, *, epsilon: float = 0.0025):
         super().__init__()
         self.epsilon: float = epsilon
 
