336375b56de1a0f0f1791c0367cb8daa5ededf23
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-29 10:40:35 +0200

    version

--- nonlinear/__init__.py
+++ nonlinear/__init__.py
@@ -3,3 +3,5 @@ from .ChannelBasedActivationFunction import ChannelBasedActivationFunction
 from .LearnableActivationFunction import LearnableActivationFunction
 from .static import *
 from .learnable import *
+
+__version__ = "0.0.14"
