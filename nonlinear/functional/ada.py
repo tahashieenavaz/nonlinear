43f67c548eb0c14992619ba84f76560ea6f652bc
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-03 15:46:57 +0200

    switches ada back to simple pytorch function

--- nonlinear/functional/ada.py
+++ nonlinear/functional/ada.py
@@ -1,26 +1,13 @@
 import torch
 
 
-class __Ada(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, x: torch.Tensor, inplace: bool = False) -> torch.Tensor:
-        ctx.save_for_backward(x.clone() if inplace else x)
-        if inplace:
-            ctx.mark_dirty(x)
-            negative_mask = x < 0
-            x[negative_mask] *= torch.exp(x[negative_mask])
-            return x
-        return torch.where(x >= 0, x, x * torch.exp(x))
-
-    @staticmethod
-    def backward(ctx, gradients: torch.Tensor):
-        (x,) = ctx.saved_tensors
-        grad_x = gradients.clone()
-        negative_mask = x < 0
-        x_neg = x[negative_mask]
-        grad_x[negative_mask] *= torch.exp(x_neg) * (1 + x_neg)
-        return grad_x, None
-
-
 def ada(x: torch.Tensor, *, inplace: bool = False) -> torch.Tensor:
-    return __Ada.apply(x, inplace)
+    if inplace:
+        negative_mask = x < 0
+        if negative_mask.any():
+            x_negative = x[negative_mask]
+            x[negative_mask] = x_negative * torch.exp(x_negative)
+        return x
+
+    exp_x = torch.exp(x)
+    return torch.where(x >= 0, x, x * exp_x)
