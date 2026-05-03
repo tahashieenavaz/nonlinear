720ee8140fb721862a6d4b5cf969a2505ae6d07f
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-03 14:16:18 +0200

    refines aoaf

--- nonlinear/functional/aoaf.py
+++ nonlinear/functional/aoaf.py
@@ -12,14 +12,42 @@ Variables:
 """
 
 
+class __AOAF(torch.autograd.Function):
+    @staticmethod
+    def forward(
+        ctx, x: torch.Tensor, b: float, c: float, inplace: bool
+    ) -> torch.Tensor:
+        ctx.b = b
+        ctx.c = c
+        ctx.reduce_dims = [d for d in range(x.ndim) if d != 1]
+        a = x.mean(dim=ctx.reduce_dims, keepdim=True)
+
+        if inplace:
+            ctx.mark_dirty(x)
+            x.sub_(b * a)
+            mask = x > 0
+            ctx.save_for_backward(mask)
+            x.relu_()
+            x.add_(c * a)
+            return x
+        else:
+            z = x - b * a
+            mask = z > 0
+            ctx.save_for_backward(mask)
+            return torch.relu(z) + c * a
+
+    @staticmethod
+    def backward(ctx, gradients: torch.Tensor):
+        (mask,) = ctx.saved_tensors
+        b = ctx.b
+        c = ctx.c
+        reduce_dims = ctx.reduce_dims
+        gradient_mask = gradients * mask
+        gradient_mean = gradients.mean(dim=reduce_dims, keepdim=True)
+        gradient_mask_mean = gradient_mask.mean(dim=reduce_dims, keepdim=True)
+        grad_x = gradient_mask + (c * gradient_mean - b * gradient_mask_mean)
+        return grad_x, None, None, None
+
+
 def aoaf(x: torch.Tensor, *, b: float = 0.17, c: float = 0.17, inplace: bool = False):
-    reduce_dims = [d for d in range(x.ndim) if d != 1]
-    a = x.mean(dim=reduce_dims, keepdim=True)
-
-    if inplace:
-        x.sub_(b * a)
-        x.relu_()
-        x.add_(c * a)
-        return x
-
-    return torch.relu(x - b * a) + c * a
+    return __AOAF.apply(x, b, c, inplace)
