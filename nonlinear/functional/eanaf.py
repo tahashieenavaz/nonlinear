6fdc9d7a295bf7758371eefe8b6376df1f76dad6
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-04 11:21:32 +0200

    makes g and h args in EANAF

--- nonlinear/functional/eanaf.py
+++ nonlinear/functional/eanaf.py
@@ -1,4 +1,5 @@
 import torch
+from typing import Callable
 
 
 def __g(x: torch.Tensor) -> torch.Tensor:
@@ -9,5 +10,5 @@ def __h(x: torch.Tensor) -> torch.Tensor:
     return torch.nn.functional.softplus(x)
 
 
-def eanaf(x: torch.Tensor) -> torch.Tensor:
-    return x * __g(__h(x))
+def eanaf(x: torch.Tensor, g: Callable = __g, h: Callable = __h) -> torch.Tensor:
+    return x * g(h(x))
