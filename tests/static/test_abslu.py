8ae26773e8555552ec25090985dab60647c603a6
tahashieenavaz <tahashieenavaz@gmail.com>
2026-08-16 17:26:02 +0200

    test: rename and expand abslu accuracy tests for functional and module

--- tests/static/test_abslu.py
+++ tests/static/test_abslu.py
@@ -1,12 +1,25 @@
 import torch
 from nonlinear.functional import abslu
+from nonlinear.static import AbsLU
 from tests.helpers import assert_equal
 
 
-def test_abslu_accuracy():
+def test_abslu_functional_accuracy():
     x = [-1.0, -0.5, 0, 0.5, 1]
     y = [0.18, 0.09, 0, 0.5, 1]
+
     for a, b in zip(x, y):
         a = torch.tensor(a)
         b = torch.tensor(b)
         assert_equal(abslu(a, alpha=0.18), b)
+
+
+def test_abslu_module_accuracy():
+    module = AbsLU()
+    x = [-1.0, -0.5, 0, 0.5, 1]
+    y = [0.18, 0.09, 0, 0.5, 1]
+
+    for a, b in zip(x, y):
+        a = torch.tensor(a)
+        b = torch.tensor(b)
+        assert_equal(module(a, alpha=0.18), b)
